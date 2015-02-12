46fea8ee65ad60de1145a191c579152c2f2db137
reto <reto@prisma>
2015-02-12 17:07:37 +0100

    Clean-Up

--- ch.quantasy.tinkerforge.tinker/ch.quantasy.tinkerforge.tinker/ch.quantasy.tinkerforge.tinker/src/ch/quantasy/tinkerforge/tinker/agent/definition/Agent.java
+++
@@ -1,8 +0,0 @@
-package ch.quantasy.tinkerforge.tinker.agent.definition;
-
-import ch.quantasy.tinkerforge.tinker.application.definition.Application;
-
-public interface Agent<E extends Application<E>> {
-	public void addApplication(E application);
-	public void removeApplication(E application);
-}
