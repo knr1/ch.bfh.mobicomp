e5657b021ec347db73eff9ec7566ca0401bd1e24
Reto E. Koenig <knr1@bfh.ch>
2015-11-04 23:31:26 +0100

    A new Approach

---
+++ ch.quantasy.iot.gateway.tinkerforge/src/main/java/ch/quantasy/iot/gateway/tinkerforge/handler/deviceHandler/joystick/status/PositionCallbackPeriodStatus.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.quantasy.iot.gateway.tinkerforge.handler.deviceHandler.joystick.status;
+
+import ch.quantasy.iot.gateway.tinkerforge.base.AHandler;
+import ch.quantasy.iot.gateway.tinkerforge.base.message.AStatus;
+import ch.quantasy.iot.gateway.tinkerforge.handler.deviceHandler.joystick.Joystick;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+
+/**
+ *
+ * @author Reto E. Koenig <reto.koenig@bfh.ch>
+ */
+public class PositionCallbackPeriodStatus extends AStatus {
+
+    public PositionCallbackPeriodStatus(AHandler deviceHandler, String statusTopic, MqttAsyncClient mqttClient) {
+	super(deviceHandler, statusTopic, "positionCallbackPeriod", mqttClient);
+	super.addDescription(Joystick.PERIOD, Long.class, "JSON", "0", "..", "" + Long.MAX_VALUE);
+    }
+}
