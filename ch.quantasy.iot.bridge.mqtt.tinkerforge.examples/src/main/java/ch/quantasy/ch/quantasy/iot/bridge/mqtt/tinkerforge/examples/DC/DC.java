5f971b3bba5d547899e253ce0e3af50d82874d1e
Reto E. Koenig <knr1@bfh.ch>
2016-01-31 19:14:46 +0100

    .

--- ch.quantasy.iot.bridge.mqtt.tinkerforge.examples/src/main/java/ch/quantasy/ch/quantasy/iot/bridge/mqtt/tinkerforge/examples/DC/DC.java
+++ ch.quantasy.iot.bridge.mqtt.tinkerforge.examples/src/main/java/ch/quantasy/ch/quantasy/iot/bridge/mqtt/tinkerforge/examples/DC/DC.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ch.quantasy.ch.quantasy.iot.bridge.mqtt.tinkerforge.examples.IMU;
+package ch.quantasy.ch.quantasy.iot.bridge.mqtt.tinkerforge.examples.DC;
 
 import java.io.IOException;
 import java.util.logging.Level;
@@ -22,17 +22,17 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
  *
  * @author Reto E. Koenig <reto.koenig@bfh.ch>
  */
-public class IMUV2 {
+public class DC {
 
     //public static final String CONNECTION = "tcp://iot.eclipse.org:1883";
     public static final String CONNECTION = "tcp://localhost:1883";
     //public static final String CONNECTION = "tcp://147.87.112.222:1883";
 
-    public static final String UID = "IMUV2-Example";
+    public static final String UID = "DC-Example";
 
     private MqttAsyncClient client;
 
-    public IMUV2() throws MqttException, InterruptedException {
+    public DC() throws MqttException, InterruptedException {
 	client = new MqttAsyncClient(CONNECTION, UID, new MemoryPersistence());
 	client.setCallback(new MQTTCallbackHandler());
     }
@@ -46,14 +46,19 @@ public class IMUV2 {
 	IMqttToken token = client.connect(options, null, new MQTTActionHandler());
 	token.waitForCompletion();
 	try {
-	    client.subscribe("iot/tf/description/IMUV2/#", 1);
-	    client.subscribe("iot/tf/localhost/4223/IMUV2/#", 1);
+	    client.subscribe("iot/tf/description/DC/#", 1);
+	    client.subscribe("iot/tf/localhost/4223/DC/#", 1);
 	    client.subscribe("iot/tf/#", 1);
 	    client.publish("iot/tf/MQTT2TF/0/intent/<" + UID + ">/stackHandler/stackAddress", "{\"hostName\":\"localhost\",\"port\":4223}".getBytes(), 1, true).waitForCompletion();
 
-	    client.publish("iot/tf/localhost/4223/IMUV2/9xblji/intent/<" + UID + ">/linearAcceleration/period", "0".getBytes(), 1, false);
+	    client.publish("iot/tf/localhost/4223/DC/e1cw8v/intent/<" + UID + ">/driveMode/value", "0".getBytes(), 1, false);
+	    client.publish("iot/tf/localhost/4223/DC/e1cw8v/intent/<" + UID + ">/enabled/value", "true".getBytes(), 1, false);
+	    client.publish("iot/tf/localhost/4223/DC/e1cw8v/intent/<" + UID + ">/currentVelocityPeriod/period", "1".getBytes(), 1, false);
+	    client.publish("iot/tf/localhost/4223/DC/e1cw8v/intent/<" + UID + ">/acceleration/value", "2000".getBytes(), 1, false);
+	    client.publish("iot/tf/localhost/4223/DC/e1cw8v/intent/<" + UID + ">/velocity/value", "7000".getBytes(), 1, false);
+
 	} catch (Exception ex) {
-	    Logger.getLogger(IMUV2.class.getName()).log(Level.SEVERE, null, ex);
+	    Logger.getLogger(DC.class.getName()).log(Level.SEVERE, null, ex);
 	}
 
     }
@@ -99,7 +104,7 @@ public class IMUV2 {
     }
 
     public static void main(String[] args) throws MqttException, InterruptedException, IOException {
-	IMUV2 button = new IMUV2();
+	DC button = new DC();
 	button.connect();
 
 	System.in.read();
